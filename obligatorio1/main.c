2a61b42faeece05987938d6e848bd4235c664d60
Sergio-Chaparro <schaparrodiazcano@gmail.com>
2022-03-23 00:18:53 +0100

    Nuevo

--- obligatorio1/main.c
+++ obligatorio1/main.c
@@ -1,1 +1,25 @@
-include
+//Programa para la simulacion del sistema solar
+
+
+#include <math.h>
+#include <stdio.h>
+
+void reescalar
+void desescalar
+void Algoritmo
+void 
+
+
+
+int main(void)
+{
+    //Declaro 
+    double t,h;
+    int N;
+
+
+    double a[][],v[][],r[][];
+
+
+
+}
